7bc2aefbd0e267fcc3ffbf0a504a889cd4a603fb
andrey_mardash <andrey.mardash@gmail.com>
2021-02-16 13:28:39 +0300

    Api rest json products

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets          # add this
+from .serializers import ProductSerializer      # add this
+from .models import Product                     # add this
+
+class ProductView(viewsets.ModelViewSet):       # add this
+  serializer_class = ProductSerializer          # add this
+  queryset = Product.objects.all()

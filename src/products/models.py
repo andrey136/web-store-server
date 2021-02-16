ca84fc066e4b4627b498632e87aa6891dbf75625
andrey_mardash <andrey.mardash@gmail.com>
2021-02-16 12:14:04 +0300

    created new model product

--- src/products/models.py
+++ src/products/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.TextField() 
+    title = models.CharField(max_length=80) 
     description = models.TextField() 
     price = models.DecimalField(max_digits=1000, decimal_places=2)
+    image = models.ImageField(upload_to='products/img')
+
+    def __str__(self):
+        return self.title
 
